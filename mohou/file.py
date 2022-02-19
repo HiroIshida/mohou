669e235cbab7c26974c25fba81aeb873307b88a5
HiroIshida <spitfire.docomo@gmail.com>
2022-02-20 03:13:04 +0900

    Extract common part from dump and load

--- mohou/file.py
+++ mohou/file.py
@@ -21,24 +21,28 @@ def get_project_dir(project_name: str) -> str:
     return dirname
 
 
-def dump_object(obj: Any, project_name: str, postfix: Optional[str] = None) -> None:
+def resolve_file_name(obj_type: Type, project_name: str, postfix: Optional[str] = None) -> str:
     dir_path = get_project_dir(project_name)
     if postfix is None:
-        file_name = osp.join(dir_path, obj.__class__.__name__ + '.pkl')
+        file_name = osp.join(dir_path, obj_type.__name__ + '.pkl')
     else:
-        file_name = osp.join(dir_path, obj.__class__.__name__ + '-' + postfix + '.pkl')
-    logger.info('dump pickle to {}'.format(file_name))
-    with open(file_name, 'wb') as f:
-        pickle.dump(obj, f)
+        file_name = osp.join(dir_path, obj_type.__name__ + '-' + postfix + '.pkl')
+    return file_name
 
 
 DataT = TypeVar('DataT')
 
 
-def load_object(obj_type: Type[DataT], project_name: str) -> DataT:
-    dir_path = get_project_dir(project_name)
-    file_name = osp.join(dir_path, obj_type.__name__ + '.pkl')
+def load_object(obj_type: Type[DataT], project_name: str, postfix: Optional[str] = None) -> DataT:
+    file_name = resolve_file_name(obj_type, project_name, postfix)
     logger.info('load pickle from {}'.format(file_name))
     with open(file_name, 'rb') as f:
         obj = pickle.load(f)
     return obj
+
+
+def dump_object(obj: Any, project_name: str, postfix: Optional[str] = None) -> None:
+    file_name = resolve_file_name(obj.__class__, project_name, postfix)
+    logger.info('dump pickle to {}'.format(file_name))
+    with open(file_name, 'wb') as f:
+        pickle.dump(obj, f)
