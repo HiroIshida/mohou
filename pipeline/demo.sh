3279eb2c71936e862a201454418de908b7db614d
HiroIshida <spitfire.docomo@gmail.com>
2022-02-21 05:05:05 +0900

    Update pipeline script

--- pipeline/demo.sh
+++ pipeline/demo.sh
@@ -2,6 +2,9 @@
 base_path=$(dirname "$(realpath $0)")
 example_path=$base_path/../example
 
-python3 $example_path/kuka_reaching.py -n 100
-python3 $example_path/train_autoencoder.py -n 3000
-python3 $example_path/train_lstm.py -n 4000
+python3 $example_path/kuka_reaching.py -n 60
+python3 $example_path/train_autoencoder.py -n 1000
+python3 $example_path/visualize_autoencoder_result.py
+python3 $example_path/train_lstm.py -n 6000
+python3 $example_path/visualize_lstm_result.py
+python3 $example_path/visualize_train_history.py
--- pipeline/test.sh
+++ pipeline/test.sh
@@ -4,11 +4,8 @@ example_path=$base_path/../example
 project_name='pipeline_test'
 
 python3 $example_path/kuka_reaching.py -pn $project_name -n 7
-
 python3 $example_path/train_autoencoder.py -pn $project_name -n 3 -timer-period 1
 python3 $example_path/visualize_autoencoder_result.py -pn $project_name
-
 python3 $example_path/train_lstm.py -pn $project_name -valid-ratio 0.5 -n 3
 python3 $example_path/visualize_lstm_result.py -pn $project_name
-
 python3 $example_path/visualize_train_history.py -pn $project_name
