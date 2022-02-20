e14f67f689b2649e72171063d245b75ff019f9a5
HiroIshida <spitfire.docomo@gmail.com>
2022-02-20 17:10:46 +0900

    Add arparse in example script

--- example/train_autoencoder.py
+++ example/train_autoencoder.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 
 from mohou.dataset import AutoEncoderDataset
@@ -7,12 +9,22 @@ from mohou.trainer import TrainCache, TrainConfig, train
 from mohou.types import MultiEpisodeChunk
 from mohou.utils import split_with_ratio
 
-project_name = 'kuka_reaching'
-chunk = MultiEpisodeChunk.load(project_name)
-dataset = AutoEncoderDataset.from_chunk(chunk)
-dataset_train, dataset_valid = split_with_ratio(dataset)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-pn', type=str, default='kuka_reaching', help='project name')
+    parser.add_argument('-n', type=int, default=3000, help='iteration number')
+    parser.add_argument('-valid-ratio', type=float, default=0.1, help='split rate for validation dataset')
+    args = parser.parse_args()
 
-tcache = TrainCache[AutoEncoder](project_name)
-model = AutoEncoder(torch.device('cpu'), AutoEncoderConfig())
-tconfig = TrainConfig(n_epoch=3)
-train(model, dataset_train, dataset_valid, tcache, config=tconfig)
+    project_name = args.pn
+    n_epoch = args.n
+    valid_ratio = args.valid_ratio
+
+    chunk = MultiEpisodeChunk.load(project_name)
+    dataset = AutoEncoderDataset.from_chunk(chunk)
+    dataset_train, dataset_valid = split_with_ratio(dataset, valid_ratio=valid_ratio)
+
+    tcache = TrainCache[AutoEncoder](project_name)
+    model = AutoEncoder(torch.device('cpu'), AutoEncoderConfig())
+    tconfig = TrainConfig(n_epoch=n_epoch)
+    train(model, dataset_train, dataset_valid, tcache, config=tconfig)
--- example/train_lstm.py
+++ example/train_lstm.py
@@ -1,3 +1,5 @@
+import argparse
+
 import torch
 
 from mohou.model.lstm import LSTMConfig
@@ -10,20 +12,30 @@ from mohou.embedding_functor import IdenticalEmbeddingFunctor
 from mohou.embedding_rule import RGBAngelVectorEmbeddingRule
 from mohou.utils import split_with_ratio
 
-project_name = 'kuka_reaching'
-chunk = MultiEpisodeChunk.load(project_name)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-pn', type=str, default='kuka_reaching', help='project name')
+    parser.add_argument('-n', type=int, default=3000, help='iteration number')
+    parser.add_argument('-valid-ratio', type=float, default=0.1, help='split rate for validation dataset')
+    args = parser.parse_args()
 
-tcache_autoencoder = TrainCache.load(project_name, AutoEncoder)
-image_embed_func = tcache_autoencoder.best_model.get_embedding_functor()
+    project_name = args.pn
+    n_epoch = args.n
+    valid_ratio = args.valid_ratio
 
-av_idendical_func = IdenticalEmbeddingFunctor(chunk.get_element_shape(AngleVector)[0])
-embed_rule = RGBAngelVectorEmbeddingRule(image_embed_func, av_idendical_func)
+    chunk = MultiEpisodeChunk.load(project_name)
 
-dataset = AutoRegressiveDataset.from_chunk(chunk, embed_rule)
-dataset_train, dataset_valid = split_with_ratio(dataset, valid_ratio=0.5)  # TODO(HiroIshida) fix it
+    tcache_autoencoder = TrainCache.load(project_name, AutoEncoder)
+    image_embed_func = tcache_autoencoder.best_model.get_embedding_functor()
 
-lstm_model = LSTM(torch.device('cpu'), LSTMConfig(embed_rule.dimension))
+    av_idendical_func = IdenticalEmbeddingFunctor(chunk.get_element_shape(AngleVector)[0])
+    embed_rule = RGBAngelVectorEmbeddingRule(image_embed_func, av_idendical_func)
 
-tconfig = TrainConfig(n_epoch=3)
-tcache = TrainCache[LSTM](project_name)
-train(lstm_model, dataset_train, dataset_valid, tcache, config=tconfig)
+    dataset = AutoRegressiveDataset.from_chunk(chunk, embed_rule)
+    dataset_train, dataset_valid = split_with_ratio(dataset, valid_ratio=valid_ratio)
+
+    lstm_model = LSTM(torch.device('cpu'), LSTMConfig(embed_rule.dimension))
+
+    tconfig = TrainConfig(n_epoch=3)
+    tcache = TrainCache[LSTM](project_name)
+    train(lstm_model, dataset_train, dataset_valid, tcache, config=tconfig)
