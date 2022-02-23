fe151edffa609eaabb3e1bc50f63a00f3eafebaa
HiroIshida <spitfire.docomo@gmail.com>
2022-02-24 02:04:26 +0900

    Update lstm visualizer

--- example/visualize_lstm_result.py
+++ example/visualize_lstm_result.py
@@ -4,12 +4,12 @@ import os
 from moviepy.editor import ImageSequenceClip
 
 from mohou.embedder import IdenticalEmbedder
-from mohou.embedding_rule import RGBAngelVectorEmbeddingRule
+from mohou.embedding_rule import RGBDAngelVectorEmbeddingRule
 from mohou.file import get_subproject_dir
 from mohou.propagator import Propagator
 from mohou.trainer import TrainCache
 from mohou.types import ElementDict, MultiEpisodeChunk
-from mohou.types import AngleVector, RGBImage
+from mohou.types import AngleVector, RGBImage, RGBDImage
 from mohou.model import AutoEncoder, LSTM
 
 from utils import add_text_to_image
@@ -29,14 +29,14 @@ if __name__ == '__main__':
     image_embed_func = tcache_autoencoder.best_model.get_embedder()
 
     av_idendical_func = IdenticalEmbedder(AngleVector, chunk.get_element_shape(AngleVector)[0])
-    embed_rule = RGBAngelVectorEmbeddingRule(image_embed_func, av_idendical_func)
+    embed_rule = RGBDAngelVectorEmbeddingRule(image_embed_func, av_idendical_func)
 
     propagator = Propagator(tcach_lstm.best_model, embed_rule)
 
     episode_data = chunk[0]
     n_feed = 10
     fed_avs = episode_data.filter_by_type(AngleVector)[:n_feed]
-    fed_images = episode_data.filter_by_type(RGBImage)[:n_feed]
+    fed_images = episode_data.filter_by_type(RGBDImage)[:n_feed]
 
     print("start lstm propagation")
     for elem_tuple in zip(fed_avs, fed_images):
@@ -44,11 +44,11 @@ if __name__ == '__main__':
     print("finish lstm propagation")
 
     elem_dict_list = propagator.predict(150)
-    pred_images = [elem_dict[RGBImage] for elem_dict in elem_dict_list]
+    pred_images = [elem_dict[RGBDImage] for elem_dict in elem_dict_list]
 
     print("adding text to images...")
-    fed_images_with_text = [add_text_to_image(im, 'fed (original)', 'blue') for im in fed_images]
-    pred_images_with_text = [add_text_to_image(im, 'predicted by lstm', 'green') for im in pred_images]
+    fed_images_with_text = [add_text_to_image(rgbd.get_primitive_image(RGBImage).numpy(), 'fed (original)', 'blue') for rgbd in fed_images]
+    pred_images_with_text = [add_text_to_image(rgbd.get_primitive_image(RGBImage).numpy(), 'predicted by lstm', 'green') for rgbd in pred_images]
 
     images_with_text = fed_images_with_text + pred_images_with_text
 
