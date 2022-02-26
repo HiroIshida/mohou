a0eaf4a45cfc20f4cae522d30d0cb0e1d89f0698
HiroIshida <spitfire.docomo@gmail.com>
2022-02-27 04:10:50 +0900

    Tweak pipeline

--- pipeline/test.sh
+++ pipeline/test.sh
@@ -3,8 +3,8 @@ base_path=$(dirname "$(realpath $0)")
 example_path=$base_path/../example
 
 function test_batch {
-    local image_type=$1
-    local project_name=$2
+    local image_type=$1Image
+    local project_name=pipeline_test_$1
     python3 $example_path/kuka_reaching.py -pn $project_name -n 7
     python3 $example_path/train_autoencoder.py -pn $project_name -n 2 -timer-period 1 -image $image_type
     python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type -n 2
@@ -12,6 +12,7 @@ function test_batch {
     python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type -n 2
     python3 $example_path/visualize_train_history.py -pn $project_name
 }
-test_batch RGBImage pipeline_test_rgb
-test_batch DepthImage pipeline_test_depth
-test_batch RGBDImage pipeline_test_rgbd
+
+test_batch RGB
+test_batch Depth
+test_batch RGBD
