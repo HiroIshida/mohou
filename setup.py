eec1acc2752d7d145ae87f78811003183b1d4aeb
HiroIshida <spitfire.docomo@gmail.com>
2022-02-27 22:49:51 +0900

    Bump to 0.0.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extras_require = {
 
 setup(
     name='mohou',
-    version='0.0.0',
+    version='0.0.1',
     description='Visuomotor imitation learning framework',
     author='Hirokazu Ishida',
     author_email='h-ishida@jsk.imi.i.u-tokyo.ac.jp',
