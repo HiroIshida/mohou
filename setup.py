1932923f09197fe10eaf54fed5cff93fdd027fa0
HiroIshida <spitfire.docomo@gmail.com>
2022-02-27 22:39:57 +0900

    Add pypi release action

--- setup.py
+++ setup.py
@@ -21,8 +21,10 @@ extras_require = {
 setup(
     name='mohou',
     version='0.0.0',
-    description='',
-    license=license,
+    description='Visuomotor imitation learning framework',
+    author='Hirokazu Ishida',
+    author_email='h-ishida@jsk.imi.i.u-tokyo.ac.jp',
+    license='MIT',
     install_requires=install_requires,
     extras_require=extras_require,
     packages=find_packages(exclude=('tests', 'docs'))
