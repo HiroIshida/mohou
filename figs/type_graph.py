5889e51b77a54dca1399a66ec720f7b62ffa7dfd
HiroIshida <spitfire.docomo@gmail.com>
2022-03-03 08:00:25 +0900

    Add type graph script

---
+++ figs/type_graph.py
@@ -0,0 +1,23 @@
+from graphviz import Digraph
+
+from mohou.types import get_all_concrete_types
+
+
+if __name__ == '__main__':
+    dg = Digraph(format='png')
+    visited_set = set()
+
+    def back_recursion(t, is_concrete=False):
+        if t in visited_set:
+            return
+        visited_set.add(t)
+
+        dg.node(t.__name__, style='filled' if is_concrete else None, fillcolor='lightgrey')
+
+        for t_parent in t.__bases__:
+            back_recursion(t_parent)
+            dg.edge(t_parent.__name__, t.__name__)
+
+    for t in get_all_concrete_types():
+        back_recursion(t, is_concrete=True)
+    dg.render('graph', cleanup=True)
