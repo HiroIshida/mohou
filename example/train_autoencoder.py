4a69b0f341aae0557974573e5f7563758c219b70
HiroIshida <spitfire.docomo@gmail.com>
2022-03-09 05:19:01 +0900

    Configure autoencoder image size

--- example/train_autoencoder.py
+++ example/train_autoencoder.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-pn', type=str, default='kuka_reaching', help='project name')
     parser.add_argument('-n', type=int, default=3000, help='iteration number')
+    parser.add_argument('-m', type=int, default=224, help='pixel number')
     parser.add_argument('-image', type=str, default='RGBImage', help='image type')
     parser.add_argument('-valid-ratio', type=float, default=0.1, help='split rate for validation dataset')
     parser.add_argument('-timer-period', type=int, default=10, help='timer period')
@@ -19,6 +20,7 @@ if __name__ == '__main__':
 
     project_name = args.pn
     n_epoch = args.n
+    n_pixel = args.m
     valid_ratio = args.valid_ratio
     timer_period = args.timer_period
     image_type: Type[ImageBase] = get_element_type(args.image)  # type: ignore
@@ -29,6 +31,6 @@ if __name__ == '__main__':
     dataset = AutoEncoderDataset.from_chunk(chunk, image_type)
 
     tcache = TrainCache[AutoEncoder](project_name, timer_period=timer_period)
-    model = AutoEncoder(AutoEncoderConfig(image_type=image_type))  # type: ignore
+    model = AutoEncoder(AutoEncoderConfig(image_type=image_type, n_pixel=n_pixel))  # type: ignore
     tconfig = TrainConfig(n_epoch=n_epoch, valid_data_ratio=valid_ratio)
     train(model, dataset, tcache, config=tconfig)
