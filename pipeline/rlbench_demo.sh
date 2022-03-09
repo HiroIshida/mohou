2f64365f8b886235be5b531bdd743b57bb851103
HiroIshida <spitfire.docomo@gmail.com>
2022-03-09 10:16:26 +0900

    Update rlbench demo pipeline

--- pipeline/rlbench_demo.sh
+++ pipeline/rlbench_demo.sh
@@ -4,14 +4,14 @@ example_path=$base_path/../example
 
 function demo_batch {
     local image_type=$1Image
-    local project_name=rlbench_$1
-    python3 $example_path/rlbench/create_dataset_mp.py -pn $project_name -n 60 -m 4
+    local project_name=rlbench_close_box
+    python3 $example_path/rlbench/create_dataset_mp.py -pn $project_name -n 105 -m 4
     python3 $example_path/train_autoencoder.py -pn $project_name -n 1000 -image $image_type
     python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type
-    python3 $example_path/train_lstm.py -pn $project_name -n 6000 -image $image_type
+    python3 $example_path/train_lstm.py -pn $project_name -n 10000 -image $image_type
     python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type
     python3 $example_path/visualize_train_history.py -pn $project_name
-    #python3 $example_path/kuka_reaching.py -pn $project_name --feedback
+    python3 $example_path/rlbench/simulate_feedback.py -pn $project_name -n 250
 }
 
 demo_batch RGBD
