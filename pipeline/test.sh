8dc3d6534f26a2c5b3599a350808867dd0a137a0
HiroIshida <spitfire.docomo@gmail.com>
2022-03-12 23:44:28 +0900

    Move example script to mohou/script

--- pipeline/test.sh
+++ pipeline/test.sh
@@ -5,13 +5,13 @@ example_path=$base_path/../example
 function test_batch {
     local image_type=$1Image
     local project_name=_pipeline_test_$1
-    python3 $example_path/kuka_reaching.py -pn $project_name -n 7
-    python3 $example_path/train_autoencoder.py -pn $project_name -n 2 -timer-period 1 -image $image_type
-    python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type -n 2
-    python3 $example_path/train_lstm.py -pn $project_name -valid-ratio 0.5 -n 2 -image $image_type
-    python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type -n 2
-    python3 $example_path/visualize_train_history.py -pn $project_name
-    python3 $example_path/kuka_reaching.py -pn $project_name --feedback
+    python3 $example_path/pybullet/create_dataset.py -pn $project_name -n 7
+    python3 -m mohou.script.train_autoencoder -pn $project_name -n 2 -timer-period 1 -image $image_type
+    python3 -m mohou.script.visualize_autoencoder_result -pn $project_name -image $image_type -n 2
+    python3 -m mohou.script.train_lstm -pn $project_name -valid-ratio 0.5 -n 2 -image $image_type
+    python3 -m mohou.script.visualize_lstm_result -pn $project_name -image $image_type -n 2
+    python3 -m mohou.script.visualize_train_history -pn $project_name
+    python3 $example_path/pybullet/simulate_feedback.py -pn $project_name
 }
 
 test_batch RGB
