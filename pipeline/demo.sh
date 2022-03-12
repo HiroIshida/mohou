07dfaff4fd6d7401f5f962af3b164b29b9da5f7a
HiroIshida <spitfire.docomo@gmail.com>
2022-03-12 23:48:04 +0900

    Update pipelines

--- pipeline/demo.sh
+++ pipeline/demo.sh
@@ -4,14 +4,15 @@ example_path=$base_path/../example
 
 function demo_batch {
     local image_type=$1Image
-    local project_name=pipeline_test_$1
-    python3 $example_path/kuka_reaching.py -pn $project_name -n 60
-    python3 $example_path/train_autoencoder.py -pn $project_name -n 1000 -image $image_type
-    python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type
-    python3 $example_path/train_lstm.py -pn $project_name -n 6000 -image $image_type
-    python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type
-    python3 $example_path/visualize_train_history.py -pn $project_name
-    python3 $example_path/kuka_reaching.py -pn $project_name --feedback
+    local project_name=pybullet_$1
+
+    python3 $example_path/pybullet/create_dataset.py -pn $project_name -n 60
+    python3 -m mohou.script.train_autoencoder -pn $project_name -n 1000 -image $image_type
+    python3 -m mohou.script.visualize_autoencoder_result -pn $project_name -image $image_type
+    python3 -m mohou.script.train_lstm -pn $project_name -n 6000 -image $image_type
+    python3 -m mohou.script.visualize_lstm_result -pn $project_name -image $image_type
+    python3 -m mohou.script.visualize_train_history -pn $project_name
+    python3 $example_path/pybullet/simulate_feedback.py -pn $project_name
 }
 
 demo_batch RGB
--- pipeline/rlbench_demo.sh
+++ pipeline/rlbench_demo.sh
@@ -6,11 +6,11 @@ function demo_batch {
     local image_type=$1Image
     local project_name=rlbench_close_box
     python3 $example_path/rlbench/create_dataset.py -pn $project_name -n 105
-    python3 $example_path/train_autoencoder.py -pn $project_name -n 1000 -image $image_type
-    python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type
-    python3 $example_path/train_lstm.py -pn $project_name -n 20000 -image $image_type
-    python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type
-    python3 $example_path/visualize_train_history.py -pn $project_name
+    python3 -m mohou.script.train_autoencoder -pn $project_name -n 1000 -image $image_type
+    python3 -m mohou.script.visualize_autoencoder_result -pn $project_name -image $image_type
+    python3 -m mohou.script.train_lstm -pn $project_name -n 20000 -image $image_type
+    python3 -m mohou.script.visualize_lstm_result -pn $project_name -image $image_type
+    python3 -m mohou.script.visualize_train_history -pn $project_name
     python3 $example_path/rlbench/simulate_feedback.py -pn $project_name -n 250
 }
 
