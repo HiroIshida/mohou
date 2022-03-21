7fc91030a832c298096568ec0557b37cc89a8bba
HiroIshida <spitfire.docomo@gmail.com>
2022-03-22 04:21:04 +0900

    Test with postfix

--- tests/test_file.py
+++ tests/test_file.py
@@ -13,7 +13,7 @@ class SampleClass:
 
 @pytest.fixture(scope='module')
 def tmp_project_name():
-    return 'pytest' + str(uuid.uuid4())
+    return 'pytest-' + str(uuid.uuid4())
 
 
 def test_dump_and_load_object(tmp_project_name):
@@ -21,6 +21,7 @@ def test_dump_and_load_object(tmp_project_name):
     dump_object(a, tmp_project_name)
     b = load_object(SampleClass, tmp_project_name)
     np.testing.assert_almost_equal(a.data, b.data)
+
     remove_project(tmp_project_name)
 
 
@@ -31,4 +32,17 @@ def test_load_objects(tmp_project_name):
     dump_object(b, tmp_project_name, str(uuid.uuid4()))
 
     objects = load_objects(SampleClass, tmp_project_name)
+    np.testing.assert_almost_equal(a.data + b.data, objects[0].data + objects[1].data)
+
+    remove_project(tmp_project_name)
+
+
+def test_load_objects_with_postfix(tmp_project_name):
+    postfix = 'hogehoge'
+    a = SampleClass(np.random.randn(10, 10))
+    b = SampleClass(np.random.randn(10, 10))
+    dump_object(a, tmp_project_name, postfix + str(uuid.uuid4()))
+    dump_object(b, tmp_project_name, postfix + str(uuid.uuid4()))
+
+    objects = load_objects(SampleClass, tmp_project_name, postfix)
     np.testing.assert_almost_equal(a.data + b.data, objects[0].data + objects[1].data)
