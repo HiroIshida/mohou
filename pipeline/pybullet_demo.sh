f34d5d1cc9e92d4446f6b40fa796d7359fb3ec74
HiroIshida <spitfire.docomo@gmail.com>
2022-03-22 19:25:20 +0000

    Train 3 times pybullet lstm

--- pipeline/pybullet_demo.sh
+++ pipeline/pybullet_demo.sh
@@ -9,7 +9,11 @@ function demo_batch {
     python3 $example_path/kuka_reaching.py -pn $project_name -n 60 -m $2
     python3 $example_path/train_autoencoder.py -pn $project_name -n 1000 -image $image_type
     python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type
-    python3 $example_path/train_lstm.py -pn $project_name -n 6000 -image $image_type
+
+    for i in $(seq 3); do
+        python3 $example_path/train_lstm.py -pn $project_name -n 10000 -image $image_type
+    done
+
     python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type
     python3 $example_path/visualize_train_history.py -pn $project_name
     python3 $example_path/kuka_reaching.py -pn $project_name --feedback -m $2
