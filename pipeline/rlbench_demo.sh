92e05858deee38ae6158464161985ef896175857
HiroIshida <spitfire.docomo@gmail.com>
2022-03-22 09:48:35 +0900

    Repeeat lstm train 3 times

--- pipeline/rlbench_demo.sh
+++ pipeline/rlbench_demo.sh
@@ -8,7 +8,11 @@ function demo_batch {
     python3 $example_path/rlbench/create_dataset.py -pn $project_name -n 105
     python3 $example_path/train_autoencoder.py -pn $project_name -n 1000 -image $image_type
     python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type
-    python3 $example_path/train_lstm.py -pn $project_name -n 20000 -image $image_type
+
+    for i in $(seq 3); do
+        python3 $example_path/train_lstm.py -pn $project_name -n 20000 -image $image_type
+    done
+
     python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type
     python3 $example_path/visualize_train_history.py -pn $project_name
     python3 $example_path/rlbench/simulate_feedback.py -pn $project_name -n 250
