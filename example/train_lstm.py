7e8d4c459014b804a0591873cff71bf0f572b1b8
HiroIshida <spitfire.docomo@gmail.com>
2022-04-16 21:03:53 +0900

    Dataset config as cmd args

--- example/train_lstm.py
+++ example/train_lstm.py
@@ -7,6 +7,7 @@ from mohou.types import MultiEpisodeChunk
 from mohou.types import AngleVector, ImageBase, get_element_type
 from mohou.model import AutoEncoder, LSTM
 from mohou.dataset import AutoRegressiveDataset
+from mohou.dataset import AutoRegressiveDatasetConfig
 from mohou.embedder import IdenticalEmbedder
 from mohou.embedding_rule import EmbeddingRule
 from mohou.utils import create_default_logger
@@ -15,6 +16,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-pn', type=str, default='kuka_reaching', help='project name')
     parser.add_argument('-n', type=int, default=3000, help='iteration number')
+    parser.add_argument('-aug', type=int, default=2, help='number of augmentation X')
+    parser.add_argument('-cov-scale', type=float, default=0.1, help='covariance scale in aug')
     parser.add_argument('-image', type=str, default='RGBImage', help='image type')
     parser.add_argument('-valid-ratio', type=float, default=0.1, help='split rate for validation dataset')
     parser.add_argument('-timer-period', type=int, default=10, help='timer period')
@@ -22,6 +25,8 @@ if __name__ == '__main__':
 
     project_name = args.pn
     n_epoch = args.n
+    n_aug = args.aug
+    cov_scale = args.cov_scale
     valid_ratio = args.valid_ratio
     timer_period = args.timer_period
     image_type: Type[ImageBase] = get_element_type(args.image)  # type: ignore
@@ -36,7 +41,8 @@ if __name__ == '__main__':
     av_idendical_func = IdenticalEmbedder(AngleVector, chunk.get_element_shape(AngleVector)[0])
     embed_rule = EmbeddingRule.from_embedders([image_embed_func, av_idendical_func])
 
-    dataset = AutoRegressiveDataset.from_chunk(chunk, embed_rule)
+    dsconfig = AutoRegressiveDatasetConfig(n_aug, cov_scale=cov_scale)
+    dataset = AutoRegressiveDataset.from_chunk(chunk, embed_rule, dsconfig)
 
     lstm_model = LSTM(LSTMConfig(embed_rule.dimension))
 
