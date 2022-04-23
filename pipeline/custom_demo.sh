f06327c9e7bb20ebba00d15f2ef358c77baa26a7
HiroIshida <spitfire.docomo@gmail.com>
2022-04-24 01:41:26 +0900

    Update demo script

---
+++ pipeline/custom_demo.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+base_path=$(dirname "$(realpath $0)")
+example_path=$base_path/../example
+
+function demo_batch {
+    local project_name=$1
+    local image_type=$2Image
+    local n_pixel=112
+    # Try different type of auto encoder
+    python3 $example_path/train_autoencoder.py -pn $project_name -n 4000 -image $image_type -latent 8
+    python3 $example_path/train_autoencoder.py -pn $project_name -n 4000 -image $image_type -latent 12
+    python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type
+
+    python3 $example_path/train_lstm.py -pn $project_name -n 40000 -image $image_type -aug 9
+    python3 $example_path/train_lstm.py -pn $project_name -n 40000 -image $image_type -aug 9
+    python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type
+    python3 $example_path/visualize_train_history.py -pn $project_name
+}
+
+demo_batch pr2_reaching RGB  # replace pr2_reaching with your own project name
+# demo_batch pr2_reaching RGBD  # replace pr2_reaching with your own project name
--- pipeline/pybullet_demo.sh
+++ pipeline/pybullet_demo.sh
@@ -10,8 +10,8 @@ function demo_batch {
     python3 $example_path/train_autoencoder.py -pn $project_name -n 1000 -image $image_type
     python3 $example_path/visualize_autoencoder_result.py -pn $project_name -image $image_type
 
-    for i in $(seq 3); do
-        python3 $example_path/train_lstm.py -pn $project_name -n 10000 -image $image_type
+    for i in $(seq 2); do
+        python3 $example_path/train_lstm.py -pn $project_name -n 10000 -image $image_type -aug 4
     done
 
     python3 $example_path/visualize_lstm_result.py -pn $project_name -image $image_type
@@ -20,5 +20,5 @@ function demo_batch {
 }
 
 demo_batch RGB 224  # must be 112 or 224
-demo_batch Depth 224
-demo_batch RGBD 224
+# demo_batch Depth 224 # comment out this if you want
+# demo_batch RGBD 224 # comment out this if you want
