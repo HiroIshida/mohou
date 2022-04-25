443498e3880455c48f18e8067fef9529c1c112d1
HiroIshida <spitfire.docomo@gmail.com>
2022-04-26 05:33:08 +0900

    Refactor example/visualize_train_history.py

--- mohou/script_utils.py
+++ mohou/script_utils.py
@@ -1,8 +1,11 @@
 import os
+import re
+import pickle
 import time
 import logging
 from logging import Logger
 from typing import Type
+import matplotlib.pyplot as plt
 
 from mohou.types import ImageBase
 from mohou.types import MultiEpisodeChunk
@@ -14,7 +17,7 @@ from mohou.dataset import AutoEncoderDataset
 from mohou.dataset import AutoEncoderDatasetConfig
 from mohou.dataset import AutoRegressiveDataset
 from mohou.dataset import AutoRegressiveDatasetConfig
-from mohou.file import get_project_dir
+from mohou.file import get_project_dir, get_subproject_dir
 from mohou.trainer import TrainCache, TrainConfig, train
 from mohou.embedding_rule import EmbeddingRule
 
@@ -75,3 +78,22 @@ def train_lstm(
     lstm_model = LSTM(model_config)
     tcache = TrainCache(project_name)  # type: ignore[var-annotated]
     train(lstm_model, dataset, tcache, config=train_config)
+
+
+def visualize_train_histories(project_name: str):
+    project_dir = get_project_dir(project_name)
+    fnames = os.listdir(project_dir)
+
+    plot_dir = get_subproject_dir(project_name, 'train_history')
+    for fname in fnames:
+        m = re.match(r'.*TrainCache.*', fname)
+        if m is not None:
+            pickle_file = os.path.join(project_dir, fname)
+
+            with open(pickle_file, 'rb') as f:
+                tcache: TrainCache = pickle.load(f)
+                fig, ax = plt.subplots()
+                tcache.visualize((fig, ax))
+                image_file = os.path.join(plot_dir, fname + '.png')
+                fig.savefig(image_file)
+                print('saved to {}'.format(image_file))
