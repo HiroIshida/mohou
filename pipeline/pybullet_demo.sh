d65d11fa20f77481467ec476ba59e44ff2d2b3bf
HiroIshida <spitfire.docomo@gmail.com>
2022-04-28 08:12:51 +0900

    Update pybullet demo

--- pipeline/pybullet_demo.sh
+++ pipeline/pybullet_demo.sh
@@ -7,12 +7,10 @@ function demo_batch {
     local project_name=pybullet_reaching_$1
     local n_pixel=$2
     python3 $example_path/kuka_reaching.py -pn $project_name -n 60 -m $2
-    python3 $example_path/train_autoencoder.py -pn $project_name -n 1000 -image $image_type
+    python3 $example_path/train_autoencoder.py -pn $project_name -n 1500 -image $image_type
     python3 $example_path/visualize_autoencoder_result.py -pn $project_name
 
-    for i in $(seq 2); do
-        python3 $example_path/train_lstm.py -pn $project_name -n 10000 -aug 4
-    done
+    python3 $example_path/train_lstm.py -pn $project_name -n 20000 -aug 9
 
     python3 $example_path/visualize_lstm_result.py -pn $project_name
     python3 $example_path/visualize_train_history.py -pn $project_name
