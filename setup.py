f9f01b57b7942f422cc64ffa6208fbd237292a63
HiroIshida <spitfire.docomo@gmail.com>
2022-05-15 01:51:19 +0900

    0.0.14

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ install_requires = [
 
 setup(
     name='mohou',
-    version='0.0.13',
+    version='0.0.14',
     description='Visuomotor imitation learning framework',
     author='Hirokazu Ishida',
     author_email='h-ishida@jsk.imi.i.u-tokyo.ac.jp',
