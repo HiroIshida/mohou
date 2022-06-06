d2f1662760f0f4685a0c7e8b3ff319b731455159
HiroIshida <spitfire.docomo@gmail.com>
2022-06-07 08:53:25 +0900

    refactor: split file of dataset

---
+++ mohou/dataset/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from mohou.dataset.autoencoder_dataset import AutoEncoderDatasetConfig, AutoEncoderDataset
+from mohou.dataset.sequence_dataset import (
+    AutoRegressiveDatasetConfig,
+    AutoRegressiveDataset,
+    MarkovControlSystemDatasetConfig,
+    MarkovControlSystemDataset,
+    WeightPolicy,
+    PWLinearWeightPolicy,
+    ConstantWeightPolicy,
+)
--- mohou/dataset/sequence_dataset.py
+++ mohou/dataset/sequence_dataset.py
@@ -2,59 +2,17 @@ import copy
 import logging
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Generic, List, Optional, Tuple, Type, Union
+from typing import List, Optional, Tuple, Union
 
 import numpy as np
 import torch
 from torch.utils.data import Dataset
 
 from mohou.encoding_rule import EncodingRule
-from mohou.types import ImageT, MultiEpisodeChunk, TerminateFlag
+from mohou.types import MultiEpisodeChunk, TerminateFlag
 from mohou.utils import assert_two_sequences_same_length
 
 logger = logging.getLogger(__name__)
-
-
-@dataclass
-class AutoEncoderDatasetConfig:
-    batch_augment_factor: int = 2  # if you have large enough RAM, set to large (like 4)
-
-    def __post_init__(self):
-        assert self.batch_augment_factor >= 0
-        logger.info("autoencoder dataset config: {}".format(self))
-
-
-@dataclass
-class AutoEncoderDataset(Dataset, Generic[ImageT]):
-    image_type: Type[ImageT]
-    image_list: List[ImageT]
-
-    def __len__(self) -> int:
-        return len(self.image_list)
-
-    def __getitem__(self, idx) -> torch.Tensor:
-        return self.image_list[idx].to_tensor()
-
-    @classmethod
-    def from_chunk(
-        cls,
-        chunk: MultiEpisodeChunk,
-        image_type: Type[ImageT],
-        augconfig: Optional[AutoEncoderDatasetConfig] = None,
-    ) -> "AutoEncoderDataset":
-
-        if augconfig is None:
-            augconfig = AutoEncoderDatasetConfig()
-
-        image_list: List[ImageT] = []
-        for episode_data in chunk:
-            image_list.extend(episode_data.get_sequence_by_type(image_type))
-
-        image_list_rand = copy.deepcopy(image_list)
-        for i in range(augconfig.batch_augment_factor):
-            image_list_rand.extend([copy.deepcopy(image).randomize() for image in image_list])
-
-        return cls(image_type, image_list_rand)
 
 
 @dataclass
