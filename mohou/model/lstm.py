d0d9ea45fbba9d6e836f301991be7eb6f9d4a8f9
HiroIshida <spitfire.docomo@gmail.com>
2022-06-14 11:00:52 +0900

    feat: enable lstm bias input

--- mohou/model/lstm.py
+++ mohou/model/lstm.py
@@ -10,34 +10,62 @@ from mohou.model.common import LossDict, ModelBase, ModelConfigBase
 @dataclass
 class LSTMConfig(ModelConfigBase):
     n_state_with_flag: int
+    n_bias: int = 0  # type independent bias input
     n_hidden: int = 200
     n_layer: int = 4
     n_output_layer: int = 1
 
 
 class LSTM(ModelBase[LSTMConfig]):
+    """
+    lstm: x_t+1 = f(x_t, x_t-1, ...)
+    lstm with bias: x_t+1 = f(x_t, x_t-1, ..., b) where b is not time dependent
+    """
+
     lstm_layer: nn.LSTM
     output_layer: nn.Sequential
 
     def _setup_from_config(self, config: LSTMConfig) -> None:
         n_state = config.n_state_with_flag
-        self.lstm_layer = nn.LSTM(n_state, config.n_hidden, config.n_layer, batch_first=True)
+        n_bias = config.n_bias
+        self.lstm_layer = nn.LSTM(
+            n_state + n_bias, config.n_hidden, config.n_layer, batch_first=True
+        )
         output_layers = []
         for _ in range(config.n_output_layer):
             output_layers.append(nn.Linear(config.n_hidden, config.n_hidden))
         output_layers.append(nn.Linear(config.n_hidden, n_state))
         self.output_layer = nn.Sequential(*output_layers)
 
-    def loss(self, sample: Tuple[torch.Tensor, torch.Tensor]) -> LossDict:
-        state_sample, weight_seqs = sample
+    def loss(self, sample: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> LossDict:
+        state_sample, bias_sample, weight_seqs = sample
+
+        # sanity check
+        n_batch, n_seq_len, n_dim = state_sample.shape
+        assert bias_sample.ndim == 2
+        assert bias_sample.shape[0] == n_batch
+        assert bias_sample.shape[1] == self.config.n_bias
+
+        assert weight_seqs.ndim == 2
+        assert weight_seqs.shape[0] == n_batch
+        assert weight_seqs.shape[1] == n_seq_len
+
+        # propagation
         state_sample_input, state_sample_output = state_sample[:, :-1], state_sample[:, 1:]
-        pred_output = self.forward(state_sample_input)
+        pred_output = self.forward(state_sample_input, bias_sample)
 
         weight_seqs_expaneded = weight_seqs[:, :-1, None].expand_as(state_sample_input)
         loss_value = torch.mean(weight_seqs_expaneded * (pred_output - state_sample_output) ** 2)
         return LossDict({"prediction": loss_value})
 
-    def forward(self, state_sample: torch.Tensor) -> torch.Tensor:
-        tmp, _ = self.lstm_layer(state_sample)
+    def forward(self, state_sample: torch.Tensor, bias_sample: torch.Tensor) -> torch.Tensor:
+        # arrange bais_sample and create concat state_sample
+        _, n_seq_len, _ = state_sample.shape
+        bias_unsqueezed = bias_sample.unsqueeze(dim=1)
+        bias_sequenced = bias_unsqueezed.expand(-1, n_seq_len, -1)
+        bias_auged_state_sample = torch.cat((state_sample, bias_sequenced), dim=2)
+
+        # propagation
+        tmp, _ = self.lstm_layer(bias_auged_state_sample)
         out = self.output_layer(tmp)
         return out
--- tests/test_lstm.py
+++ tests/test_lstm.py
@@ -7,18 +7,21 @@ def test_lstm():
     n_sample = 10
     n_seq_len = 100
     n_dim_with_flag = 7
+    n_dim_bias = 4
 
-    config = LSTMConfig(n_dim_with_flag)
+    config = LSTMConfig(n_dim_with_flag, n_bias=n_dim_bias)
     model: LSTM = LSTM(config)
 
     state_sample = torch.randn(n_sample, n_seq_len, n_dim_with_flag).float()
+    bias_sample = torch.randn(n_sample, n_dim_bias).float()
 
     # test forward
-    state_prop = model.forward(state_sample)
+    state_prop = model.forward(state_sample, bias_sample)
     assert state_prop.shape == (n_sample, n_seq_len, n_dim_with_flag)
 
     # test loss
-    sample = (state_sample, torch.rand(n_sample, n_seq_len))
+    weight_sample = torch.rand(n_sample, n_seq_len)
+    sample = (state_sample, bias_sample, weight_sample)
     loss_dict = model.loss(sample)
     assert "prediction" in loss_dict
     assert loss_dict.total() == loss_dict["prediction"]
