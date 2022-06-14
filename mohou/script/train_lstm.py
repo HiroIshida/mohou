9ad76d6765ed42a877daf304272a01689cccb61b
Hirokazu Ishida <38597814+HiroIshida@users.noreply.github.com>
2022-06-14 20:21:33 +0900

    feat: layer and hidden are configure (#78)
    * feat: layer and hidden state are configurable
    
    * Fix

--- mohou/script/train_lstm.py
+++ mohou/script/train_lstm.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
     parser.add_argument("-pn", type=str, default=setting.primary_project_name, help="project name")
     parser.add_argument("-n", type=int, default=3000, help="iteration number")
     parser.add_argument("-aug", type=int, default=2, help="number of augmentation X")
+    parser.add_argument("-hidden", type=int, default=200, help="number of hidden state of lstm")
+    parser.add_argument("-layer", type=int, default=4, help="number of layers of lstm")
     parser.add_argument("-cov-scale", type=float, default=0.1, help="covariance scale in aug")
     parser.add_argument(
         "-valid-ratio", type=float, default=0.1, help="split rate for validation dataset"
@@ -22,6 +24,8 @@ if __name__ == "__main__":
     project_name = args.pn
     n_epoch = args.n
     n_aug = args.aug
+    n_hidden = args.hidden
+    n_layer = args.layer
     cov_scale = args.cov_scale
     valid_ratio = args.valid_ratio
     warm_start = args.warm
@@ -29,7 +33,7 @@ if __name__ == "__main__":
     logger = create_default_logger(project_name, "lstm")  # noqa
 
     encoding_rule = create_default_encoding_rule(project_name)
-    model_config = LSTMConfig(encoding_rule.dimension)
+    model_config = LSTMConfig(encoding_rule.dimension, n_hidden=n_hidden, n_layer=n_layer)
     dataset_config = AutoRegressiveDatasetConfig(n_aug=n_aug, cov_scale=cov_scale)
     train_config = TrainConfig(n_epoch=n_epoch, valid_data_ratio=valid_ratio)
     train_lstm(
