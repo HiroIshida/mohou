d9c8067aa76b32462e469112749708fed252f741
HiroIshida <spitfire.docomo@gmail.com>
2022-06-27 04:01:02 +0900

    Visualize chimera ae

--- mohou/script/visualize_autoencoder_result.py
+++ mohou/script/visualize_autoencoder_result.py
@@ -1,7 +1,7 @@
 import argparse
 
 from mohou.default import auto_detect_autoencoder_type
-from mohou.model.autoencoder import VariationalAutoEncoder
+from mohou.model import AutoEncoder, Chimera, VariationalAutoEncoder
 from mohou.script_utils import (
     visualize_image_reconstruction,
     visualize_variational_autoencoder,
@@ -14,13 +14,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-pn", type=str, default=setting.primary_project_name, help="project name")
     parser.add_argument("-n", type=int, default=5, help="number of visualization")
+    parser.add_argument("--chimera", action="store_true", help="use chimera")
     args = parser.parse_args()
     project_name = args.pn
     n_vis = args.n
 
-    ae_type = auto_detect_autoencoder_type(project_name)
-    model = TrainCache.load(project_name, ae_type).best_model
-    assert model is not None
+    if args.chimera:
+        ae_type = AutoEncoder  # TODO(HiroIshida): currently fixed
+        chimera = TrainCache.load(project_name, Chimera).best_model
+        assert chimera is not None
+        model = chimera.ae
+    else:
+        ae_type = auto_detect_autoencoder_type(project_name)
+        model = TrainCache.load(project_name, ae_type).best_model
+        assert model is not None
 
     chunk = MultiEpisodeChunk.load(project_name)
     visualize_image_reconstruction(project_name, chunk, model, n_vis)
