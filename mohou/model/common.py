08d551c22c24d5267cd3edf11e2ebd43526f2e43
Hirokazu Ishida <38597814+HiroIshida@users.noreply.github.com>
2022-07-15 04:53:47 +0900

    feat: get n_parameter of model (#116)

--- mohou/model/common.py
+++ mohou/model/common.py
@@ -95,5 +95,15 @@ class ModelBase(nn.Module, Generic[ModelConfigT], ABC):
     def loss(self, sample: Any) -> LossDict:
         pass
 
+    @property
+    def n_parameter(model):
+        total_params = 0
+        for name, parameter in model.named_parameters():
+            if not parameter.requires_grad:
+                continue
+            params = parameter.numel()
+            total_params += params
+        return total_params
+
 
 ModelT = TypeVar("ModelT", bound=ModelBase)
