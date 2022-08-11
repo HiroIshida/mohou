083434fa3345cef22047392b38bebb8ba5764e16
HiroIshida <spitfire.docomo@gmail.com>
2022-08-11 18:29:20 +0900

    Refactor class structure

--- example/pybullet_complex_task/pickup_book.py
+++ example/pybullet_complex_task/pickup_book.py
@@ -30,14 +30,25 @@ class Environment:
     urdf_path: str
 
     def __post_init__(self):
-        self.randomize()
+        # reset robot
+        robot_id = self.handles["robot"]
+        for joint_id in self.joint_to_id_table.values():
+            pb.resetJointState(robot_id, joint_id, 0.0, targetVelocity=0.0)
 
-    def randomize(self):
+        # reset base
+        for object_id in self.handles.values():
+            pb.resetBaseVelocity(
+                object_id, linearVelocity=(0.0, 0.0, 0.0), angularVelocity=(0.0, 0.0, 0.0)
+            )
+
+        self.randomize_world()
+
+    def randomize_world(self):
         x_pos = 0.45 + np.random.randn() * 0.05
         y_pos = 0.1 + np.random.randn() * 0.05
         yaw = np.random.randn() * 0.1
         np.random.randn(3) * 0.06
-        self.set_pose("box1", (x_pos, y_pos, 0.03), (yaw, 0, 0))
+        self.set_pose("box1", (x_pos, y_pos, 0.05), (yaw, 0, 0))
         self.set_pose("box2", (x_pos, y_pos, 0.08), (yaw, 0, 0))
 
     def set_pose(
@@ -76,8 +87,8 @@ class Environment:
         pb.configureDebugVisualizer(pb.COV_ENABLE_GUI, 0)
         pb.setGravity(0, 0, -10)
 
-        box_id = create_box(BoxConfig(size=(0.3, 0.2, 0.05), rgba=(1, 0.7, 0.7, 1.0)))
-        box2_id = create_box(BoxConfig(size=(0.3, 0.2, 0.05), rgba=(0.7, 1.0, 0.7, 1.0)))
+        box_id = create_box(BoxConfig(size=(0.3, 0.2, 0.08), rgba=(1, 0.7, 0.7, 1.0)))
+        box2_id = create_box(BoxConfig(size=(0.3, 0.2, 0.04), rgba=(0.7, 1.0, 0.7, 1.0)))
 
         handles = {"robot": robot_id, "box1": box_id, "box2": box2_id}
 
@@ -95,24 +106,40 @@ class Environment:
 
         return cls(handles, joint_table, link_table, str(panda_urdf_path))
 
-    def set_joint_angle(self, joint_name: str, angle: float, real_time: bool, gain: float = 0.3):
+    def set_joint_angle(self, joint_name: str, angle: float, gain: float = 1.0):
         joint_id = self.joint_to_id_table[joint_name]
-        if real_time:
-            pb.setJointMotorControl2(
-                bodyIndex=self.handles["robot"],
-                jointIndex=joint_id,
-                controlMode=pb.POSITION_CONTROL,
-                targetPosition=angle,
-                targetVelocity=0.0,
-                force=300,
-                positionGain=gain,
-                velocityGain=1.0,
-                maxVelocity=1.0,
-            )
-        else:
-            pb.resetJointState(self.handles["robot"], joint_id, angle)
+        pb.setJointMotorControl2(
+            bodyIndex=self.handles["robot"],
+            jointIndex=joint_id,
+            controlMode=pb.POSITION_CONTROL,
+            targetPosition=angle,
+            targetVelocity=0.0,
+            force=300,
+            positionGain=gain,
+            velocityGain=1.0,
+            maxVelocity=1.0,
+        )
+
+    def set_angle_vetor(self, robot: "PandaModel"):
+        for name in robot.control_joint_names:
+            joint = robot.robot_model.__dict__[name]
+            joint_id = self.joint_to_id_table[name]
+            pb.resetJointState(self.handles["robot"], joint_id, joint.joint_angle())
+
+    def send_angel_vector(self, robot: "PandaModel"):
+        for name in robot.control_joint_names:
+            joint = robot.robot_model.__dict__[name]
+            self.set_joint_angle(name, joint.joint_angle())
+
+    def change_gripper_position(self, pos: float):
+        assert pos > -1e-3 and pos < 0.08 + 1e-3
+        name1 = "panda_finger_joint1"
+        name2 = "panda_finger_joint2"
+        self.set_joint_angle(name1, pos * 0.5, True)
+        self.set_joint_angle(name2, pos * 0.5, True)
 
     def wait_interpolation(self, sleep: float = 0.0, callback: Optional[Callable] = None) -> None:
+        counter = 0
         while True:
             time.sleep(sleep)
             pb.stepSimulation()
@@ -123,8 +150,10 @@ class Environment:
                 _, vel, _, _ = pb.getJointState(self.handles["robot"], joint_id)
                 velocities.append(vel)
             vel_max = np.max(np.abs(velocities))
+            counter += 1
             if vel_max < 0.05:
                 break
+        print(counter)
 
     def step(self, n: int, sleep: float = 0.0) -> None:
         for _ in range(n):
@@ -155,22 +184,6 @@ class PandaModel:
             joint = self.robot_model.__dict__[name]
             joint.joint_angle(angle)
 
-    def send_angel_vector(self, env: Environment, real_time: bool = False):
-        for name in self.control_joint_names:
-            joint = self.robot_model.__dict__[name]
-            env.set_joint_angle(name, joint.joint_angle(), real_time)
-
-    def change_gripper_position(self, pos: float, env: Optional[Environment] = None):
-        assert pos > -1e-3 and pos < 0.08 + 1e-3
-        name1 = "panda_finger_joint1"
-        name2 = "panda_finger_joint2"
-        self.robot_model.__dict__[name1].joint_angle(pos * 0.5)
-        self.robot_model.__dict__[name2].joint_angle(pos * 0.5)
-
-        if env is not None:
-            env.set_joint_angle(name1, pos * 0.5, True)
-            env.set_joint_angle(name2, pos * 0.5, True)
-
     @property
     def control_joint_names(self) -> List[str]:
         return ["panda_joint{}".format(i + 1) for i in range(7)]
@@ -193,70 +206,70 @@ class PandaModel:
 
 env = Environment.create()
 robot = PandaModel.from_urdf(env.urdf_path)
-robot.set_angle_vector([0.0, 0.7, 0.0, -0.5, 0.0, 1.3, -0.8])
-robot.send_angel_vector(env)
 
-for _ in range(1000):
-    pb.stepSimulation()
+for _ in range(30):
+    env.randomize_world()
+    robot.set_angle_vector([0.0, 0.7, 0.0, -0.5, 0.0, 1.3, -0.8])
+    env.set_angle_vetor(robot)
 
-# pre-push pose
-target = env.get_skrobot_coords("box2").copy_worldcoords()
-target.translate([0.0, 0.15, 0.02])
-target.rotate(np.pi * 0.5, "y")
+    # pre-push pose
+    target = env.get_skrobot_coords("box2").copy_worldcoords()
+    target.translate([0.0, 0.15, 0.04])
+    target.rotate(np.pi * 0.5, "y")
 
-robot.solve_ik(target)
-robot.send_angel_vector(env, real_time=True)
-env.wait_interpolation()
+    robot.solve_ik(target)
+    env.send_angel_vector(robot)
+    env.wait_interpolation()
+    # time.sleep(2)
 
-# push
-target.translate([0.0, -0.06, 0.0], wrt="world")
-robot.solve_ik(target)
-robot.send_angel_vector(env, real_time=True)
-env.wait_interpolation()
+    # push
+    target.translate([0.0, -0.06, 0.0], wrt="world")
+    robot.solve_ik(target)
+    env.send_angel_vector(robot)
+    env.wait_interpolation()
 
-# go up
-target.translate([0.0, 0.0, 0.1], wrt="world")
-robot.solve_ik(target)
-robot.send_angel_vector(env, real_time=True)
-env.wait_interpolation()
+    # go up
+    target.translate([0.0, 0.0, 0.1], wrt="world")
+    robot.solve_ik(target)
+    env.send_angel_vector(robot)
+    env.wait_interpolation()
 
-# move around
-target = env.get_skrobot_coords("box2").copy_worldcoords()
-target.translate([0.0, -0.25, 0.12])
-target.rotate(np.pi * 0.5, "y")
-robot.solve_ik(target)
-robot.send_angel_vector(env, real_time=True)
-env.wait_interpolation()
+    # move around
+    target = env.get_skrobot_coords("box2").copy_worldcoords()
+    target.translate([0.0, -0.25, 0.12])
+    target.rotate(np.pi * 0.5, "y")
+    robot.solve_ik(target)
+    env.send_angel_vector(robot)
+    env.wait_interpolation()
 
-target = env.get_skrobot_coords("box2").copy_worldcoords()
-target.translate([0.0, -0.18, 0.04])
-target.rotate(np.pi * 0.5, "y")
-target.rotate(np.pi * 0.1, "z")
-robot.solve_ik(target)
-robot.send_angel_vector(env, real_time=True)
-env.wait_interpolation()
+    target = env.get_skrobot_coords("box2").copy_worldcoords()
+    target.translate([0.0, -0.18, 0.04])
+    target.rotate(np.pi * 0.5, "y")
+    target.rotate(np.pi * 0.1, "z")
+    robot.solve_ik(target)
+    env.send_angel_vector(robot)
+    env.wait_interpolation()
 
-target = env.get_skrobot_coords("box2").copy_worldcoords()
-target.translate([0.0, -0.18, 0.04])
-target.rotate(np.pi * 0.5, "y")
-target.rotate(np.pi * 0.35, "z")
-robot.solve_ik(target)
-robot.send_angel_vector(env, real_time=True)
-env.wait_interpolation()
+    target = env.get_skrobot_coords("box2").copy_worldcoords()
+    target.translate([0.0, -0.18, 0.04])
+    target.rotate(np.pi * 0.5, "y")
+    target.rotate(np.pi * 0.35, "z")
+    robot.solve_ik(target)
+    env.send_angel_vector(robot)
+    env.wait_interpolation()
 
-# open and grasp
-robot.change_gripper_position(0.07, env)
-env.wait_interpolation()
-robot.move_end_pos([0.09, 0.0, 0])
-robot.send_angel_vector(env, real_time=True)
-env.step(300, sleep=0.01)
-robot.change_gripper_position(0.02, env)
-env.wait_interpolation(sleep=0.01)
+    # open and grasp
+    env.change_gripper_position(0.07)
+    env.wait_interpolation()
+    robot.move_end_pos([0.09, 0.0, 0])
+    env.send_angel_vector(robot)
+    env.step(300, sleep=0.0)
+    env.change_gripper_position(0.02)
+    env.step(300, sleep=0.0)
+    # env.wait_interpolation(sleep=0.01)
 
-# lift
-robot.move_end_pos(pos=(0, 0, 0.15), wrt="world")
-robot.move_end_rot(np.pi * 0.15, "z")
-robot.send_angel_vector(env, real_time=True)
-env.wait_interpolation()
-
-time.sleep(30)
+    # lift
+    robot.move_end_pos(pos=(0, 0, 0.15), wrt="world")
+    robot.move_end_rot(np.pi * 0.15, "z")
+    env.send_angel_vector(robot)
+    env.wait_interpolation()
