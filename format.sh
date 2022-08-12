e711c1546a58e149729432fc1d4bb89d3f98a98d
HiroIshida <spitfire.docomo@gmail.com>
2022-08-12 21:09:04 +0900

    chore: black no specific version

--- format.sh
+++ format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 find . -name "*py"|xargs autoflake -i --remove-all-unused-imports --remove-unused-variables --ignore-init-module-imports 
 isort . 
-black --required-version 22.3.0 .
+black .
 flake8 .
