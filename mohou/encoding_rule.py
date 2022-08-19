fix: handle static sequence in covariance balancer computation (#174)
* fix: check if covariance matrix is valid

* Make encoding_function be deterministic and injective

* Fix markov dataset test

* [CI must fail] add test case which must caused error

* Relax covariance check condition

* Fix: check only max eig value

* Update

mohou/encoding_rule.py
@@ -191,6 +191,16 @@ class CovarianceBalancer:
                 if cov.ndim == 0:  # unfortunately, np.cov return 0 dim array instead of 1x1
                     cov = np.expand_dims(cov, axis=0)
                     cov = np.array([[cov.item()]])
+
+            determinant = np.linalg.det(cov)
+            if determinant < 10 ** (-4 * dim):
+                # TODO: It is maybe better to assert by this condition. But, for testing purpose,
+                # this check is too strict sometimes...
+                message = "warn: cov {} has too small determinant {}. Probably some of the element is almost static.".format(
+                    cov, determinant
+                )
+                logger.warning(message)
+
             means.append(mean)
             covs.append(cov)
         return means, covs
@@ -201,7 +211,24 @@ class CovarianceBalancer:
         for cov in covs:
             eig_values, _ = np.linalg.eig(cov)
             max_eig = max(eig_values)
-            primary_stds.append(np.sqrt(max_eig))
+            max_std = np.sqrt(max_eig)
+
+            # Note that np.cov(pts.T) outputs some non 0 value even if the input points
+            # are all at a single point (degenerated). So, max_eig > 0.0 cannot check
+            # the data degeneration. Thus...
+            if max_eig < 1e-10:
+                # The value 0.1 is determined somewhat ad-hoc manner.
+                # Such case happens primary for GripperState, which typically varies from
+                # 0.0 to 0.1.
+                max_eig_adhoc = 0.1
+                logger.warning(
+                    "because max_eig {} is too small, we force set the value to {}".format(
+                        max_eig, max_eig_adhoc
+                    )
+                )
+                max_eig = max_eig_adhoc
+
+            primary_stds.append(max_std)
         scaled_primary_stds = [std / max(primary_stds) for std in primary_stds]
         return scaled_primary_stds
 

tests/test_dataset.py
@@ -160,7 +160,7 @@ def test_markov_control_system_dataset(image_av_bundle_uneven):  # noqa
         n_len_ground_truth = sum([len(seq) - 1 for seq in controls_seq]) * (n_aug + 1)
         assert len(dataset) == n_len_ground_truth
 
-        # test the first content (because cov = 0.0 ..)
+        # test the first content of the dataset (Note that there is no randomization)
         inp_ctrl, inp_obs, out_obs = dataset[0]
 
         if diff_as_control:
@@ -170,11 +170,11 @@ def test_markov_control_system_dataset(image_av_bundle_uneven):  # noqa
         np.testing.assert_almost_equal(inp_obs, observations_seq[0][0])
         np.testing.assert_almost_equal(out_obs, observations_seq[0][1])
 
-        # test the last content (because cov = 0.0 ...)
+        # test the last content of the dataset (Note that there is no randomization)
         inp_ctrl, inp_obs, out_obs = dataset[-1]
         if diff_as_control:
             np.testing.assert_almost_equal(inp_ctrl, controls_seq[-1][1] - controls_seq[-1][0])
         else:
             np.testing.assert_almost_equal(inp_ctrl, controls_seq[-1][0])
-        np.testing.assert_almost_equal(inp_obs, observations_seq[-1][-1])
-        np.testing.assert_almost_equal(out_obs, observations_seq[-1][-2])
+        np.testing.assert_almost_equal(inp_obs, observations_seq[-1][-2])
+        np.testing.assert_almost_equal(out_obs, observations_seq[-1][-1])

tests/test_encoding_rule.py
@@ -46,6 +46,13 @@ def sample_covariance_balancer():
     return balancer
 
 
+def test_covariance_balancer_with_static_values():
+    a = np.random.randn(1000, 3)
+    a[:, 1] *= 0.0
+    a[:, 2] *= 0.0
+    CovarianceBalancer.from_feature_seqs(a, {Vector1: 2, Vector2: 1})
+
+
 def test_covariance_balancer(sample_covariance_balancer):
     balancer: CovarianceBalancer = sample_covariance_balancer
 
@@ -97,9 +104,14 @@ def create_encoding_rule_for_image_av_bundle(
     image_type = [t for t in bundle.types() if issubclass(t, ImageBase)].pop()
     dims_image: Tuple[int, int, int] = bundle.get_element_shape(image_type)  # type: ignore
 
+    def forward_impl(img_tensor: torch.Tensor):
+        # whatever function as long as it's deterministic injective function
+        vec = img_tensor[0, 0, 0, :dim_image_encoded].float()
+        return vec
+
     f1 = ImageEncoder(
         image_type,
-        lambda img: torch.zeros(dim_image_encoded),
+        forward_impl,
         lambda vec: torch.zeros(tuple(reversed(dims_image))),
         dims_image,
         dim_image_encoded,
@@ -112,11 +124,16 @@ def create_encoding_rule_for_image_av_bundle(
 
 
 def test_encoding_rule_apply_to_edict(rgbd_image_bundle):  # noqa
-    pass
+    dim_image_encoded = 5
+
+    def forward_impl(img_tensor: torch.Tensor):
+        # whatever function as long as it's deterministic injective function
+        vec = img_tensor[0, 0, 0, :dim_image_encoded].float()
+        return vec
 
     f1 = ImageEncoder(
         RGBDImage,
-        lambda img: torch.zeros(5),
+        forward_impl,
         lambda vec: torch.zeros((4, 30, 30)),
         (30, 30, 4),
         5,