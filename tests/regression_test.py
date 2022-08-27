dcb6fc7faed17e09e91a9fca5bc866e76a791569
HiroIshida <spitfire.docomo@gmail.com>
2022-08-28 05:49:50 +0900

    test: add regression test using md5 hash

--- tests/regression_test.py
+++ tests/regression_test.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import pickle
+from hashlib import md5
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import gdown
 
-from mohou.default import create_default_propagator
+from mohou.default import create_default_encoding_rule, create_default_propagator
 from mohou.model import LSTM, VariationalAutoEncoder
 from mohou.propagator import Propagator
 from mohou.trainer import TrainCache
@@ -14,6 +16,9 @@ from mohou.types import AngleVector, EpisodeBundle
 
 def test_episode_bundle_loading(project_path: Path):
     bundle = EpisodeBundle.load(project_path)
+    bundle_hash = md5(pickle.dumps(bundle)).hexdigest()
+    print(bundle_hash)
+    assert bundle_hash == "e2fdc191d7c8ac6acc40f8cad7241222", "hash value of bundle does not match"
 
     # test bundle is ok (under construction)
     bundle.plot_vector_histories(AngleVector, project_path)
@@ -24,6 +29,17 @@ def test_episode_bundle_loading(project_path: Path):
     episode.save_debug_gif(gif_path)
     assert gif_path.exists()
     print("bundle loading succeeded")
+
+
+def test_encoding_rule(project_path: Path):
+    encoding_rule = create_default_encoding_rule(project_path)
+    bundle = EpisodeBundle.load(project_path)
+    arr_list = encoding_rule.apply_to_episode_bundle(bundle)
+    arr_list_hash = md5(pickle.dumps(arr_list)).hexdigest()
+    print(arr_list_hash)
+    assert (
+        arr_list_hash == "8c0f71c150f2c3b5fbda03a3b0b7471a"
+    ), "hash value of encoded arr does not match"
 
 
 def test_trained_model_replay(project_path: Path):
@@ -61,4 +77,5 @@ if __name__ == "__main__":
 
         # test main
         test_episode_bundle_loading(pp)
+        test_encoding_rule(pp)
         test_trained_model_replay(pp)
