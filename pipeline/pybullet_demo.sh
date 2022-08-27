997617aa71c1162b1bcdc5994a6191e3559f2c30
HiroIshida <spitfire.docomo@gmail.com>
2022-08-28 06:28:06 +0900

    fix: remove comment-out in pybullet demo

--- pipeline/pybullet_demo.sh
+++ pipeline/pybullet_demo.sh
@@ -6,13 +6,13 @@ function demo_batch {
     local image_type=$1Image
     local project_name=pybullet_reaching_$1
     local n_pixel=$2
-    #python3 $example_path/kuka_reaching.py -pn $project_name -n 60 -m $2
-    #python3 -m mohou.script.train_autoencoder -pn $project_name -n 1500 -image $image_type --vae -aug 0
-    #python3 -m mohou.script.visualize_autoencoder_result -pn $project_name
+    python3 $example_path/kuka_reaching.py -pn $project_name -n 60 -m $2
+    python3 -m mohou.script.train_autoencoder -pn $project_name -n 1500 -image $image_type --vae -aug 0
+    python3 -m mohou.script.visualize_autoencoder_result -pn $project_name
 
-    #python3 -m mohou.script.train_lstm -pn $project_name -n 20000 -aug 9
+    python3 -m mohou.script.train_lstm -pn $project_name -n 20000 -aug 9
 
-    #python3 -m mohou.script.visualize_lstm_result -pn $project_name
+    python3 -m mohou.script.visualize_lstm_result -pn $project_name
     python3 -m mohou.script.visualize_train_history -pn $project_name
     python3 $example_path/kuka_reaching.py -pn $project_name --feedback -m $2
 }
