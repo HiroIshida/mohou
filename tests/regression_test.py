76a118e5c71a1e537020c817fe5625d78f0c5490
Hirokazu Ishida <38597814+HiroIshida@users.noreply.github.com>
2022-08-30 08:45:54 +0900

    test: add regression test for models and propagator (#200)
    * test: add regresion test of propagator comparing sum value
    
    * Add regression test for model comparing output sum
    
    * ci: show pip freeze
    
    * Consider numerical error due to float32
    
    * Exclude RGBImage because it has uint8 type (incompatible other float
    values)

--- tests/regression_test.py
+++ tests/regression_test.py
@@ -4,12 +4,13 @@ from pathlib import Path
 from tempfile import TemporaryDirectory
 
 import numpy as np
+import torch
 
 from mohou.default import create_default_encoding_rule, create_default_propagator
 from mohou.model import LSTM, VariationalAutoEncoder
 from mohou.propagator import Propagator
 from mohou.trainer import TrainCache
-from mohou.types import AngleVector, EpisodeBundle
+from mohou.types import AngleVector, EpisodeBundle, EpisodeData, RGBImage, TerminateFlag
 
 
 def test_episode_bundle_loading(project_path: Path):
@@ -37,19 +38,64 @@ def test_encoding_rule(project_path: Path):
     assert abs(sum_value - (-2799.7209571566077)) < 1e-4, "arr list sum does not match"
 
 
-def test_trained_model_replay(project_path: Path):
-    # check at least model loading success
-    TrainCache.load(project_path, LSTM)
-    TrainCache.load(project_path, VariationalAutoEncoder)
+def test_ae_model(project_path: Path):
+    ae_cache = TrainCache.load(project_path, VariationalAutoEncoder)
+    vae = ae_cache.best_model
 
-    # check propagator can be constructed using LSTM and autoencoder
-    prop = create_default_propagator(project_path, Propagator)
-    bundle = EpisodeBundle.load(project_path).get_untouch_bundle()
+    create_default_encoding_rule(project_path)
+    bundle = EpisodeBundle.load(project_path)
     episode = bundle[0]
-    for i in range(10):
+    image_seq = episode.get_sequence_by_type(RGBImage)
+
+    # do not put many image. float rooted numerical error becomes large
+    arr = torch.stack([image.to_tensor() for image in image_seq[0:3]])
+    ret = vae.forward(arr)
+    sum_value = torch.sum(ret).item()
+    print(sum_value)
+    assert abs(sum_value - (415137.75)) < 1e-4, "ae sum does not match"
+
+
+def test_lstm_model(project_path: Path):
+    lstm_cache = TrainCache.load(project_path, LSTM)
+    lstm = lstm_cache.best_model
+    lstm.forward
+
+    encoding_rule = create_default_encoding_rule(project_path)
+    bundle = EpisodeBundle.load(project_path)
+    episode = bundle[0]
+    arr = encoding_rule.apply_to_episode_data(episode)
+
+    dummy_sample = torch.from_numpy(arr).float().unsqueeze(dim=0)
+    n_batch, n_seqlen, n_dim = dummy_sample.shape
+    dummy_context = torch.randn(n_batch, 0)
+    val, _ = lstm.forward(dummy_sample, dummy_context)
+    sum_value = torch.sum(val).item()
+    print(sum_value)
+    assert abs(sum_value - (31.875625610351562)) < 1e-4, "lstm sum does not match"
+
+
+def test_propagator(project_path: Path):
+    prop: Propagator = create_default_propagator(project_path, Propagator)
+    bundle = EpisodeBundle.load(project_path)
+    episode = bundle[0]
+
+    for i in range(40):
         prop.feed(episode[i])
-    prop.predict(5)
-    print("model loading succeeded")
+    edict_seq = prop.predict(2)
+    episode = EpisodeData.from_edict_list(edict_seq, check_terminate_flag=False)
+
+    episode.get_sequence_by_type(AngleVector)
+    episode.get_sequence_by_type(RGBImage)
+    episode.get_sequence_by_type(TerminateFlag)
+
+    sum_value = 0.0
+    for elem_type in [AngleVector, TerminateFlag]:
+        # not including RGB because its type is uint8
+        seq = episode.get_sequence_by_type(elem_type)  # type: ignore
+        for elem in seq:
+            sum_value += np.sum(elem.numpy())
+    print(sum_value)
+    assert abs(sum_value - (0.2111293077468872)) < 1e-6, "sum does not match"
 
 
 if __name__ == "__main__":
@@ -66,4 +112,6 @@ if __name__ == "__main__":
         # test main
         test_episode_bundle_loading(pp)
         test_encoding_rule(pp)
-        test_trained_model_replay(pp)
+        test_ae_model(pp)
+        test_lstm_model(pp)
+        test_propagator(pp)
