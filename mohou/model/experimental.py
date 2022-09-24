c7eb44b6bce1707f11a3cc22be327acee6478dea
Hirokazu Ishida <38597814+HiroIshida@users.noreply.github.com>
2022-09-25 06:20:24 +0900

    feat(experimental): add DisentangleLSTM model (#224)
    * feat(experimental): add DisentangleLSTM model
    
    * feat(propagator): add DisentangleLSTMPropagator
    
    * feat(visualize_lstm_result): include DisentagleLSTMPropagator

--- mohou/model/experimental.py
+++ mohou/model/experimental.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 
 from mohou.model.common import LossDict, ModelBase, ModelConfigBase
+from mohou.model.lstm import LSTM, LSTMConfig
 
 
 def build_linear_layers(
@@ -153,3 +154,75 @@ class ProportionalModel(ModelBase[ProportionalModelConfig]):
         z_post = (1.0 - p_value) * z
         sample_post = self.decoder(z_post)
         return sample_post
+
+
+@dataclass
+class DisentangleLSTMConfig(ModelConfigBase):
+    n_state_with_flag: int
+    n_bottleneck: int = 10
+    n_fc_depth: int = 3
+    n_hidden: int = 200
+    n_layer: int = 1
+
+
+class DisentangleLSTM(ModelBase[DisentangleLSTMConfig]):
+    encoder: nn.Module
+    decoder: nn.Module
+    lstm: LSTM
+
+    def _setup_from_config(self, config: DisentangleLSTMConfig) -> None:
+        layers = build_linear_layers(
+            config.n_state_with_flag, config.n_bottleneck, 100, config.n_fc_depth, activation="tanh"
+        )
+        self.encoder = nn.Sequential(*layers)
+
+        layers = build_linear_layers(
+            config.n_bottleneck, config.n_state_with_flag, 100, config.n_fc_depth, activation="tanh"
+        )
+        self.decoder = nn.Sequential(*layers)
+
+        conf = LSTMConfig(config.n_bottleneck, 0, config.n_hidden, config.n_layer, n_output_layer=1)
+        self.lstm = LSTM(conf)
+
+    def loss(self, sample: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> LossDict:
+        # NOTE: episode index and static context is not supported in this model
+        _, X, _ = sample
+
+        n_batch, n_seq_len, n_dim = X.shape
+        X0 = X[:, :-1, :]
+        X1 = X[:, 1:, :]
+
+        X0_reshaped = X0.reshape(-1, n_dim)
+        Z0_reshaped = self.encoder(X0_reshaped)
+        X0_reconst_reshaped = self.decoder(Z0_reshaped)
+        X0_reconst = X0_reconst_reshaped.reshape(n_batch, n_seq_len - 1, n_dim)
+
+        Z0 = Z0_reshaped.reshape(n_batch, n_seq_len - 1, self.config.n_bottleneck)
+
+        empty_context = torch.empty((n_batch, 0)).to(Z0.device)
+        Z1_est, _ = self.lstm.forward(Z0, empty_context)
+        Z1_est_reshaped = Z1_est.reshape(-1, self.config.n_bottleneck)
+        X1_est_reshaped = self.decoder(Z1_est_reshaped)
+        X1_est = X1_est_reshaped.reshape(n_batch, n_seq_len - 1, n_dim)
+
+        reconstruction_loss = nn.MSELoss()(X0_reconst, X0)
+        prediction_loss = nn.MSELoss()(X1_est, X1)
+
+        return LossDict({"reconstruction": reconstruction_loss, "prediction": prediction_loss})
+
+    def forward(self, X0: torch.Tensor) -> torch.Tensor:
+        n_batch, n_seq_len, n_dim = X0.shape
+
+        X0_reshaped = X0.reshape(-1, n_dim)
+
+        Z0_reshaped = self.encoder(X0_reshaped)
+
+        Z0 = Z0_reshaped.reshape(n_batch, n_seq_len, self.config.n_bottleneck)
+
+        empty_context = torch.empty((n_batch, 0)).to(Z0.device)
+        Z1_est, _ = self.lstm.forward(Z0, empty_context)
+
+        Z1_est_reshaped = Z1_est.reshape(-1, self.config.n_bottleneck)
+        X1_est_reshaped = self.decoder(Z1_est_reshaped)
+        X1_est = X1_est_reshaped.reshape(n_batch, n_seq_len, n_dim)
+        return X1_est
--- mohou/script/visualize_lstm_result.py
+++ mohou/script/visualize_lstm_result.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from mohou.file import get_project_path
 from mohou.propagator import (
     ChimeraPropagator,
+    DisentangleLSTMPropagator,
     LSTMPropagator,
     PBLSTMPropagator,
     PropagatorBase,
@@ -19,6 +20,7 @@ class Propagatorselection(Enum):
     pblstm = PBLSTMPropagator
     chimera = ChimeraPropagator
     proportional = ProportionalModelPropagator
+    disentangle = DisentangleLSTMPropagator
 
 
 if __name__ == "__main__":
