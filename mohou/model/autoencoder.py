7a7f85f1dc534a4a26d0b2f6eeb1bbf1f0c2de01
HiroIshida <spitfire.docomo@gmail.com>
2022-10-06 11:39:19 +0900

    feat(autoencoder): add batch normalization layer

--- mohou/model/autoencoder.py
+++ mohou/model/autoencoder.py
@@ -38,102 +38,142 @@ def create_encoder_decoder_layers(
     if n_pixel == 224:
         encoder_layers = [
             nn.Conv2d(n_channel, 8, 3, padding=1, stride=(2, 2)),  # 112x112
+            nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
             nn.Conv2d(8, 16, 3, padding=1, stride=(2, 2)),  # 56x56
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.Conv2d(16, 32, 3, padding=1, stride=(2, 2)),  # 28x28
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 64, 3, padding=1, stride=(2, 2)),  # 14x14
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 128, 3, padding=1, stride=(2, 2)),  # 7x7
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.Conv2d(128, 256, 3, padding=1, stride=(2, 2)),  # 4x4
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.Flatten(),
             nn.Linear(256 * 16, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Linear(512, n_bottleneck),
+            nn.BatchNorm1d(n_bottleneck),
             nn.ReLU(inplace=True),
         ]
         decoder_layers = [
             nn.Linear(n_bottleneck, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 256 * 16),
+            nn.BatchNorm1d(256 * 16),
             nn.ReLU(inplace=True),
             Reshape(-1, 256, 4, 4),
             nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(32, 16, 4, stride=2, padding=1),
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(16, 8, 4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(8, n_channel, 4, stride=2, padding=1),
+            nn.BatchNorm2d(n_channel),
             nn.Sigmoid(),
         ]
     elif n_pixel == 112:
         encoder_layers = [
             nn.Conv2d(n_channel, 8, 3, padding=1, stride=(2, 2)),  # 56x56
+            nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
             nn.Conv2d(8, 16, 3, padding=1, stride=(2, 2)),  # 28x28
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.Conv2d(16, 32, 3, padding=1, stride=(2, 2)),  # 14x14
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.Conv2d(32, 64, 3, padding=1, stride=(2, 2)),  # 7x7
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 128, 3, padding=1, stride=(2, 2)),  # 4x4
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.Flatten(),
             nn.Linear(128 * 16, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Linear(512, n_bottleneck),
+            nn.BatchNorm1d(n_bottleneck),
             nn.ReLU(inplace=True),
         ]
         decoder_layers = [
             nn.Linear(n_bottleneck, 512),
+            nn.BatchNorm1d(512),
             nn.ReLU(inplace=True),
             nn.Linear(512, 128 * 16),
+            nn.BatchNorm1d(128 * 16),
             nn.ReLU(inplace=True),
             Reshape(-1, 128, 4, 4),
             nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(32, 16, 4, stride=2, padding=1),
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(16, 8, 4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(8, n_channel, 4, stride=2, padding=1),
+            nn.BatchNorm2d(n_channel),
             nn.Sigmoid(),
         ]
     else:
         encoder_layers = [
             nn.Conv2d(n_channel, 8, 3, padding=1, stride=(2, 2)),  # 14x14
+            nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
             nn.Conv2d(8, 16, 3, padding=1, stride=(2, 2)),  # 7x7
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),  # 64x4x4
             nn.Conv2d(16, 32, 3, padding=1, stride=(2, 2)),  # 4x4
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),  # 64x4x4
             nn.Flatten(),
             nn.Linear(32 * 16, 8 * 16),
+            nn.BatchNorm1d(8 * 16),
             nn.ReLU(inplace=True),
             nn.Linear(8 * 16, n_bottleneck),
+            nn.BatchNorm1d(n_bottleneck),
             nn.ReLU(inplace=True),
         ]
         decoder_layers = [
             nn.Linear(n_bottleneck, 8 * 16),
+            nn.BatchNorm1d(8 * 16),
             nn.ReLU(inplace=True),
             nn.Linear(8 * 16, 32 * 16),
+            nn.BatchNorm1d(32 * 16),
             nn.ReLU(inplace=True),
             Reshape(-1, 32, 4, 4),
             nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1),
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(16, 8, 4, stride=2, padding=1),
+            nn.BatchNorm2d(8),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(8, n_channel, 4, stride=2, padding=1),
+            nn.BatchNorm2d(n_channel),
             nn.Sigmoid(),
         ]
     return encoder_layers, decoder_layers
@@ -251,9 +291,11 @@ class VariationalAutoEncoder(AutoEncoderBase[ImageT]):
             self.channel(), config.n_pixel, config.n_bottleneck
         )
         encoder_layers.pop()  # remove relu
+        encoder_layers.pop()  # remove batch layer
         encoder_layers.pop()  # remove dense
 
-        encoder_last_dense: nn.Linear = encoder_layers[-2]  # type: ignore
+        encoder_last_dense: nn.Linear = encoder_layers[-3]  # type: ignore
+        assert isinstance(encoder_last_dense, nn.Linear)
         out_dim = encoder_last_dense.out_features
 
         self.encoder_module = nn.Sequential(*encoder_layers)
