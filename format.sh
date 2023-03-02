2aafac01bdec1dec7adb15b434233cd7ca74ed20
HiroIshida <spitfire.docomo@gmail.com>
2023-03-03 05:58:42 +0900

    ci: fix black version

--- format.sh
+++ format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 find . -name "*py"|xargs python3 -m autoflake -i --remove-all-unused-imports --remove-unused-variables --ignore-init-module-imports
 python3 -m isort .
-python3 -m black .
+python3 -m black --required-version 22.6.0 .
 python3 -m flake8 .
